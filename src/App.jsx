c5038d7253cae6599ac673b4e11a19736cdc2bf7
Aradhya Paudel <poudlearadhya@gmail.com>
2026-03-23 21:04:09 +0545

    Finished fixing the double button press and added tries method

--- src/App.jsx
+++ src/App.jsx
@@ -19,19 +19,22 @@ function App() {
             
             const newVarC = Math.floor((varA + varB) / 2);
             setVarC(newVarC);
-            if (num == varC) {
+            if (num == newVarC) {
                     setTries(tries + 1);
-                    document.querySelector('.subheading').textContent = `The number is ${varC} and it took ${tries} tries to guess it!`;
-                    
+                document.querySelector('.subheading').textContent = `The number is ${newVarC} and it took ${tries + 1} tries to guess it!`;
+                document.querySelector('.mainInput').value = "";
+                setVarA(0);
+                setVarB(100);
+                setNumber(0);
                     setTries(0);
                 }
-                else if (num < varC) {
+                else if (num < newVarC) {
                     setVarB(newVarC);
                     setTries(tries + 1);
                     
 
                 }
-                else if (num > varC) {
+                else if (num > newVarC) {
                     setVarA(newVarC);
                     setTries(tries + 1);
                     
@@ -53,8 +56,9 @@ function App() {
         <input
           type="text"
           placeholder="Enter a number between 1 and 100"
-          className="bg-primary text-secondary placeholder:text-secondary focus:text-secondary border-none rounded-xl placeholder:text-center placeholder:text-3xl focus:border-none w-140 h-20 mt-20 text-center text-3xl "
-          onChange={handleChange}
+          className="mainInput bg-primary text-secondary placeholder:text-secondary focus:text-secondary border-none rounded-xl placeholder:text-center placeholder:text-3xl focus:border-none w-140 h-20 mt-20 text-center text-3xl "
+                  onChange={handleChange}
+                  
         />
         <button className="bg-secondary text-primary font-bold text-3xl rounded-xl w-40 h-20 ml-5 mt-20 hover:bg-secondary/80 transition duration-300" onClick={handleButtonClick}>
           Enter
